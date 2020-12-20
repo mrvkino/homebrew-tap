3deaa1c8b90585ddbc666905057017ad45e056e1
Michel Rogers-Vallee <mrvkino@gmail.com>
2020-12-20 11:09:16 -0500

    🍱 feat (cask): add Custer.app cask

---
+++ Casks/mrvkino-custer.rb
@@ -0,0 +1,22 @@
+cask "mrvkino-custer" do
+  version "1.1.0"
+  sha256 "79f10f142658781a6b0f3e703071fd720a5321c15e56264e0a9b9110fc0cd64b"
+
+  url "https://github.com/exelban/custer/releases/download/v#{version}/Custer.dmg"
+  name "Custer"
+  desc "macOS streaming radio in your menu bar"
+  homepage "https://github.com/exelban/custer"
+
+  app "Custer.app"
+
+  uninstall launchctl: "eu.exelban.custer.LaunchAtLogin"
+
+  uninstall quit: "eu.exelban.custer"
+
+  zap trash: [
+    "~/Library/Application Scripts/eu.exelban.custer.LaunchAtLogin",
+    "~/Library/Caches/eu.exelban.custer",
+    "~/Library/Containers/eu.exelban.custer.LaunchAtLogin",
+    "~/Library/Preferences/eu.exelban.custer.plist"
+  ]
+end
